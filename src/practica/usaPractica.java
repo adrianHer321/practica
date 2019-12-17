ff4913965df844a7fbe0f7997774d7d3030b9b94
WIN <WIN@DESKTOP-K3UGAK8>
2019-12-17 07:53:03 -0500

    Hola

---
+++ src/practica/usaPractica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+/**
+ *
+ * @author WIN
+ */
+public class usaPractica {
+    public static void main(String[] args) {
+        Practica p=new Practica();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+    }
+    
+}
